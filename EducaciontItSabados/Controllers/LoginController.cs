abac3df910fee3802ade6a45cbe71722fbee68b2
Marcio Abriola <marcioabriola@gmail.com>
2022-10-08 11:58:48 -0300

    Se sube la clase

---
+++ EducaciontItSabados/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducaciontItSabados.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
